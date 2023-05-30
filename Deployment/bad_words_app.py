5f8d35ed4a51027b091eca63420570ddd3218906
R Panneer Selvam <98254459+Selvamsmail@users.noreply.github.com>
2023-05-30 23:00:45 +0530

    Update bad_words_app.py

--- Deployment/bad_words_app.py
+++ Deployment/bad_words_app.py
@@ -88,6 +88,7 @@ def badword_prediction(input_data):
 
     predictions = model.predict(padded_sequence)[0][0]
     threshold = 0.5
+    predictions = np.exp(raw_prob) / (np.exp(raw_prob) + 1)
     confidence = predictions
     predictions = ('it is a Bad word with a confidence of '+str(predictions)+' %' if predictions >= threshold else 'it is not a Bad word' )
        
